3bb77bc9f56f25da9d4b6915042db93c455582fe
Sapna Ankenagare <sankenagare@conexussg.com>
2025-12-30 14:50:36 -0600

    Financial Views

---
+++ views/DW_NS_X_FIN_CASHSALE_V.sql
@@ -0,0 +1,30 @@
+SELECT 
+   'cashsale' T_TYPE,
+    t.tranid TRAN_ID,
+      t.ID InternalID,
+    tl.id LINE_ID,
+    t.trandate TRAN_DATE,
+    tl.trandisplayname TRAN_NAME,
+    tl.subsidiary SUBSIDIARY_ID,
+    tl.location LOCATION_ID,
+    tl.class DIVISION_ID,
+    tl.department DEPARTMENT_ID,
+    tl.entity ENTITY_ID,
+    tl.item ITEM_ID,
+    t.postingperiod POSTING_PERIOD_ID,
+    tl.account ACCOUNT_ID,
+    a.acctnumber ACCOUNT_NUMBER,
+    a.accttype ACCOUNT_TYPE,
+    a.description ACCOUNT_DESC,
+    NVL(tl.quantity,null)*-1 QTY,
+    null EXT_COST,
+    NVL(tl.accountingbook_credit,0) - NVL(tl.accountingbook_debit,0) ACCTG_AMOUNT,
+      tl.memo MEMO
+ FROM  dw_ns_cash_sale_f t
+JOIN dw_ns_cash_sale_lines_f tl
+    ON t.id = tl.transaction
+JOIN dw_ns_account_d a
+    ON tl.account = a.id
+WHERE 
+--SUBSTR(a.acctnumber,1,1) IN ('4','5','6','7','8','9') AND 
+t.posting = 'T'
